7fc8cfe3a9ffa063ce900c29b8cdbf1bf2712675
Josh <jreginaldo@protonmail.com>
2022-04-06 18:04:09 -0500

    Add javascript functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 function init() {
-//add your javascrip between these two lines of code
-
-
-    window.addEventListener('load', init);
+    // Add event listener to 'AlertMe' div
+    function buttonAlert() {
+        var textOutput = document.getElementById("textoutput"); // Text below button
+        var userInput = document.getElementById("entryinput").value; // Text field
+        textOutput.innerHTML = userInput; // Display user input below button
+        alert("Josh Reginaldo: " + userInput); // Display user input in an alert box
+    }
+    var button = document.getElementById("entrybutton");
+    button.addEventListener("click", buttonAlert);
 }
+window.addEventListener('load', init);
